37fad6f2f01b2340dc6304228bb4c8399fbfb0c6
Adhithia Irvan Rachmawan <adhitia.irvan@sicepat.com>
2022-12-02 22:10:07 +0700

    add module kubernetes

---
+++ module/kubernetes/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  backend "gcs" {}
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.43.1"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  region = var.region
+  zone = var.zone
+  project = var.project
+  credentials = var.credentials
+}
+
+resource "google_container_cluster" "default" {
+  name     = var.name
+
+  remove_default_node_pool = true
+  initial_node_count       = var.node_count
+}
+
+resource "google_container_node_pool" "default" {
+  name       = "${var.name}-node-pool"
+  cluster    = google_container_cluster.default.name
+  node_count = 1
+
+  node_config {
+    preemptible  = false
+    machine_type = var.instance_type
+  }
+}
---
+++ module/kubernetes/vars.tf
@@ -0,0 +1,27 @@
+variable "region" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
+
+variable "project" {
+  type = string
+}
+
+variable "credentials" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "node_count" {
+  type = number
+}
+
+variable "instance_type" {
+  type = string
+}
